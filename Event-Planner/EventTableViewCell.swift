bb6924c5aa9dd84d57b89bb5c515d36b0f476d9c
Prahlad Anand <anprahlad@gmail.com>
2020-04-12 20:37:27 -0700

    viewEvents basic story completed (cell view isn't working properly)

---
+++ Event-Planner/EventTableViewCell.swift
@@ -0,0 +1,29 @@
+//
+//  EventTableViewCell.swift
+//  Event-Planner
+//
+//  Created by Prahlad Anand on 4/12/20.
+//  Copyright © 2020 cs490group. All rights reserved.
+//
+
+import UIKit
+
+class EventTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var eventLocationField: UILabel!
+    @IBOutlet weak var eventNameField: UILabel!
+    @IBOutlet weak var eventDescriptionField: UILabel!
+    @IBOutlet weak var eventDateTimeField: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- Event-Planner/EventsTableViewController.swift
+++ Event-Planner/EventsTableViewController.swift
@@ -7,40 +7,108 @@
 //
 
 import UIKit
+import Firebase
+import FirebaseDatabase
+
+class Event {
+    let ref: DatabaseReference?
+    var dateTime: String?
+    var eventHost: String?
+    var eventName: String?
+    var location: String?
+    
+    init (ref: DatabaseReference?, dateTime: String?, eventHost: String?, eventName: String?, location: String?) {
+        self.dateTime = dateTime
+        self.eventHost = eventHost
+        self.eventName = eventName
+        self.location = location
+        self.ref = ref
+    }
+    init?(snapshot: DataSnapshot) {
+        guard
+            let value = snapshot.value as? [String: AnyObject],
+            let dateTime = value["DateTime"] as? String,
+            let eventHost = value["EventHost"] as? String,
+            let eventName = value["EventName"] as? String,
+            let location = value["Location"] as? String
+        else {
+            return nil
+        }
+        
+        
+        self.ref = snapshot.ref
+        self.dateTime = dateTime
+        self.eventHost = eventHost
+        self.eventName = eventName
+        self.location = location
+        
+    }
+    
+}
+
 
 class EventsTableViewController: UITableViewController {
+    
+    var ref: DatabaseReference!
+    var myEvents = [Event]()
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        ref = Database.database().reference()
+        let events = self.ref.child("Events")
 
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
 
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
         // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        events.queryOrdered(byChild: "EventName").observe(.value, with: {snapshot in
+            var newEvents: [Event] = []
+            for child in snapshot.children {
+                if let snapshot = child as? DataSnapshot,
+                    let newEvent = Event(snapshot: snapshot) {
+                    print(newEvent)
+                    newEvents.append(newEvent)
+                }
+            }
+            
+            self.myEvents = newEvents
+            self.tableView.reloadData()
+        })
     }
+    
+    
+    
+    
 
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return myEvents.count
     }
 
-    /*
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "EventTableViewCell", for: indexPath) as! EventTableViewCell
+        let curEvent = myEvents[indexPath.row]
+        
+        cell.eventNameField.text = curEvent.eventName
+        cell.eventLocationField.text = curEvent.location
+        cell.eventDescriptionField.text = ""
+        cell.eventDateTimeField.text = curEvent.dateTime
 
         // Configure the cell...
 
         return cell
     }
-    */
+    
 
     /*
     // Override to support conditional editing of the table view.
