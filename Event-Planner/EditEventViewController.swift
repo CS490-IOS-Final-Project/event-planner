c3ed53cc0cdb32313ad204f7822ca6fc51023f29
Prahlad Anand <anprahlad@gmail.com>
2020-04-26 12:29:59 -0700

    finished edit event (need to add event description to DB)

--- Event-Planner/EditEventViewController.swift
+++ Event-Planner/EditEventViewController.swift
@@ -8,10 +8,120 @@
 
 import UIKit
 import Firebase
+import GooglePlaces
 
-class EditEventViewController: UIViewController {
+class EditEventViewController: UIViewController, GMSAutocompleteViewControllerDelegate {
+    var event: Event!
+    var ref: DatabaseReference!
+    var placeId: String?
     
+    var eventId: String?
+    
+    @IBOutlet weak var eventLocation: UITextField!
+    @IBOutlet weak var eventName: UITextField!
+    @IBOutlet weak var eventDateTime: UIDatePicker!
+    @IBOutlet weak var eventDescription: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
 
+        // Do any additional setup after loading the view.
+        ref = Database.database().reference()
+        eventLocation.isUserInteractionEnabled = false
+        
+        eventLocation.text = event.location as String?
+        eventName.text = event.eventName as String?
+        eventDescription.text = "This is a filler description!" as String?
+        
+        self.placeId = event.locationId
+        
+    }
+    
+    @IBAction func saveEvent(_ sender: UIButton!) {
+      
+        if (eventName.text == nil || eventName.text!.isEmpty) {
+            print("empty event name")
+            return
+        }
+        
+        if (eventLocation.text == nil || eventLocation.text!.isEmpty) {
+            print("empty location")
+            return
+        }
+        
+        if (eventDescription.text == nil || eventDescription.text!.isEmpty) {
+            print("empty description")
+            return
+        }
+        
+        let dateFormatter = DateFormatter()
+
+        dateFormatter.dateStyle = DateFormatter.Style.short
+        dateFormatter.timeStyle = DateFormatter.Style.short
+        
+        let strDate = dateFormatter.string(from: eventDateTime.date)
+        
+        print(self.event.ref?.key)
+        
+        let currEvent = self.ref.child("Events").child((self.event.ref?.key)!)
+        
+        let update = [
+            "EventName": eventName.text!,
+            "Location": eventLocation.text!,
+            "DateTime": strDate,
+            "LocationID": placeId
+        ]
+        
+        currEvent.updateChildValues(update)
+        
+        navigationController?.popViewController(animated: true)
+    }
+    
+    @IBAction func addLocation(_ sender: Any) {
+        let autocompleteController = GMSAutocompleteViewController()
+        autocompleteController.delegate = self
+
+        // Specify the place data types to return.
+        let fields: GMSPlaceField = GMSPlaceField(rawValue: UInt(GMSPlaceField.name.rawValue) |
+          UInt(GMSPlaceField.placeID.rawValue) | UInt(GMSPlaceField.formattedAddress.rawValue))!
+        autocompleteController.placeFields = fields
+
+        // Specify a filter.
+        let filter = GMSAutocompleteFilter()
+        filter.type = .address
+        autocompleteController.autocompleteFilter = filter
+
+        // Display the autocomplete view controller.
+        present(autocompleteController, animated: true, completion: nil)
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    
+    func viewController(_ viewController: GMSAutocompleteViewController, didAutocompleteWith place: GMSPlace) {
+      eventLocation.text = place.formattedAddress
+      self.placeId = place.placeID
+      dismiss(animated: true, completion: nil)
+    }
+
+    func viewController(_ viewController: GMSAutocompleteViewController, didFailAutocompleteWithError error: Error) {
+      // TODO: handle the error.
+      print("Error: ", error.localizedDescription)
+    }
+
+    // User canceled the operation.
+    func wasCancelled(_ viewController: GMSAutocompleteViewController) {
+      dismiss(animated: true, completion: nil)
+    }
+    
+    
+    
     /*
     // MARK: - Navigation
 
--- Event-Planner/HostedEventsTableViewController.swift
+++ Event-Planner/HostedEventsTableViewController.swift
@@ -112,18 +112,18 @@ class HostedEventsTableViewController: UITableViewController {
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
-//    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-//        // Get the new view controller using segue.destination.
-//        // Pass the selected object to the new view controller.
-//        let cell = sender as! UITableViewCell
-//        let indexPath = tableView.indexPath(for: cell)!
-//        let event = myEvents[indexPath.row]
-//
-//        let detailsViewController = segue.destination as! EditEventViewController
-//
-//        detailsViewController.event = event
-//        tableView.deselectRow(at: indexPath, animated: true)
-//    }
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+        let cell = sender as! UITableViewCell
+        let indexPath = tableView.indexPath(for: cell)!
+        let event = myEvents[indexPath.row]
+
+        let editViewController = segue.destination as! EditEventViewController
+
+        editViewController.event = event
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
     
 
 }
